88ff7001ca2ffd5b94eecaddc920c320dae5a1ec
Esmeralda Arreguin-Martinez <esmerarre@gmail.com>
2025-11-21 13:44:53 -0800

    wave1 complete

--- src/adagrams.js
+++ src/adagrams.js
@@ -1,6 +1,19 @@
 export const drawLetters = () => {
   // Implement this method for wave 1
-    const letterPool = createLetterPool();
+  const letterPool = createLetterPool();
+  const selectedIndex = [];
+  const pickedLetters = [];
+  for (let i = 0; i <= 9; i++) {
+    let randSelection = randomMaxInclusive(0, letterPool.length - 1);
+    while (selectedIndex.includes(randSelection)) {
+      randSelection = randomMaxInclusive(0, letterPool.length - 1);
+    };
+    selectedIndex.push(randSelection);
+    const randomLetter = letterPool[randSelection];
+    pickedLetters.push(randomLetter);
+
+  }
+  return pickedLetters;
 
 };
 
@@ -18,18 +31,18 @@ export const highestScoreFrom = (words) => {
 
 const createLetterPool = () => {
   const letterFrequencies = {
-    A : 9,	N : 6,
-    B : 2,	O : 8,
-    C : 2,	P : 2,
-    D : 4,	Q : 1,
-    E : 12,	R : 6,
-    F : 2,	S : 4,
-    G : 3,	T : 6,
-    I : 9,	V : 2,
-    J : 1,	W : 2,
-    K : 1,	X : 1,
-    L : 4,	Y : 2,
-    M : 2,	Z : 1
+    A: 9, N: 6,
+    B: 2, O: 8,
+    C: 2, P: 2,
+    D: 4, Q: 1,
+    E: 12, R: 6,
+    F: 2, S: 4,
+    G: 3, T: 6,
+    I: 9, V: 2,
+    J: 1, W: 2,
+    K: 1, X: 1,
+    L: 4, Y: 2,
+    M: 2, Z: 1
   };
   const letterPool = [];
   for (const [key, value] of Object.entries(letterFrequencies)) {
@@ -38,3 +51,7 @@ const createLetterPool = () => {
   }
   return letterPool;
 };
+
+function randomMaxInclusive(min, max) {
+  return Math.floor((Math.random()) * (max - min + 1)) + min;
+}
